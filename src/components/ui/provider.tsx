ee16cc2db900bdc0eb486a7c33f58c2dd07df624
João Lopes <joaovlopesmartins@gmail.com>
2025-03-24 20:41:03 -0300

    chore: adjusting theme chakra provider

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,15 +1,24 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import {
   ColorModeProvider,
   type ColorModeProviderProps,
-} from "./color-mode"
+} from "./color-mode";
+
+// Cria um tema customizado com base no defaultSystem, sobrescrevendo a configuração
+const customTheme = {
+  ...defaultSystem,
+  config: {
+    initialColorMode: "light", // ou "dark"
+    useSystemColorMode: false, // desabilita a utilização do modo do sistema
+  },
+};
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={customTheme}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
-  )
+  );
 }
