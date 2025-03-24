0d1546f3979c30566152dba76e6aac81ef594874
João Lopes <joaovlopesmartins@gmail.com>
2025-03-24 00:54:15 -0300

    chore: config setup deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "liga-extremo-sul",
+  base: "/iga-extremo-sul",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
